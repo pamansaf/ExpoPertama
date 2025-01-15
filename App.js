92e6844c004d19d1bea777d7a85491bca2c0a92f
pamansaf <safwan.ssy@gmail.com>
2025-01-15 20:51:32 +0700

    install expo, tailwind, buat file babel.config.js

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-blue-500">
+        Open up App.js to start working on your app!
+      </Text>
+      <Text className="text-red-500 pt-10">
+        Open up App.js to start working on your app!
+      </Text>
+      <Text className="text-slate-800 font-bold pt-10">
+        Open up App.js to start working on your app!
+      </Text>
+      <Text className="text-black font-bold pt-2">
+        Open up App.js to start working on your app!
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
