1a751c8fdd20026897eb22ca89d5d3ca0ab13140
pamansaf <safwan.ssy@gmail.com>
2025-01-16 22:58:31 +0700

    RN Reanimated

--- app/about.jsx
+++ app/about.jsx
@@ -1,17 +1,40 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
-
+import { Link, useRouter } from "expo-router";
+import Animated, {
+  FadeIn,
+  FadeOut,
+  SlideInLeft,
+  SlideInRight,
+  ZoomIn,
+  ZoomOut,
+} from "react-native-reanimated";
 const about = () => {
+  const router = useRouter();
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl font-bold pb-3">About</Text>
-      <Link href="/" asChild>
-        <TouchableOpacity className="bg-red-500 py-2 px-3 rounded-md">
-          <Text className="text-white">Go to home</Text>
+    <Animated.View
+      // entering={FadeIn.duration(500)}
+      // exiting={FadeOut.duration(500)}
+      entering={ZoomIn.duration(300)}
+      exiting={ZoomOut.duration(300)}
+      className="flex-1 justify-center items-center"
+    >
+      <Text className="bg-sky-300 p-3 text-black-500 text-2xl font-bold mb-3">
+        About
+      </Text>
+      {/* <Link href="/about" asChild>
+        <TouchableOpacity className="bg-sky-500 py-2 px-3 rounded-md">
+          <Text className="text-white">Go ttttto again about</Text>
         </TouchableOpacity>
-      </Link>
-    </View>
+      </Link> */}
+
+      <TouchableOpacity
+        className="bg-green-500 py-2 px-3 rounded-md"
+        onPress={() => router.push("/")}
+      >
+        <Text className="text-white">Go to About</Text>
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,40 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
-
+import { Link, useRouter } from "expo-router";
+import Animated, {
+  FadeIn,
+  FadeOut,
+  SlideInLeft,
+  SlideInRight,
+  ZoomIn,
+  ZoomOut,
+} from "react-native-reanimated";
 const Home = () => {
+  const router = useRouter();
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-red-500 text-2xl font-bold pb-3">belajar expo</Text>
-      <Link href="/about" asChild>
+    <Animated.View
+      // entering={FadeIn.duration(300)}
+      // exiting={FadeOut.duration(300)}
+      entering={ZoomIn.duration(300)}
+      exiting={ZoomOut.duration(300)}
+      className="flex-1 justify-center items-center"
+    >
+      <Text className="bg-yellow-300 p-3 text-red-500 text-2xl font-bold mb-3">
+        Home
+      </Text>
+      {/* <Link href="/about" asChild>
         <TouchableOpacity className="bg-sky-500 py-2 px-3 rounded-md">
           <Text className="text-white">Go ttttto again about</Text>
         </TouchableOpacity>
-      </Link>
-    </View>
+      </Link> */}
+
+      <TouchableOpacity
+        className="bg-sky-500 py-2 px-3 rounded-md"
+        onPress={() => router.push("/about")}
+      >
+        <Text className="text-white">Go to About</Text>
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
 
